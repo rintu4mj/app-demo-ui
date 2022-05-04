f0c5866458ae4a567d7febaec61a40ff6207007a
Rintu Mazumder <Rintu.Mazumder@neudesic.com>
2022-05-04 15:13:41 +0530

    aded

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     
-    axios.get(`http://app-demo-service:8080/api/tutorials`)
+    axios.get(`http://app-demo-ms-service:8081/api/status`)
     .then(res => {
       const lists = res.data;
       setList(lists);
